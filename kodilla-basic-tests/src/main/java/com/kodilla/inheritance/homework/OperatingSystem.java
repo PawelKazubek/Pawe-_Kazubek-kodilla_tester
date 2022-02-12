8c43c27126b276a61233b573867187bdcc15c22f
Pawel Kazubek <p.kazubek@gmail.com>
2022-02-12 13:19:15 +0100

    Initial version

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
@@ -0,0 +1,14 @@
+package com.kodilla.inheritance.homework;
+
+public class OperatingSystem {
+    private int publicationyear;
+
+    public void turnOn() {
+        System.out.println("Publication year is turned on");
+    }
+
+    public void turnOff() {
+        System.out.println("Publication year is turned off");
+    }
+
+}
