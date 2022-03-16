d645916236f2a6291db1801117a5d84142ffb66e
Pawel Kazubek <p.kazubek@gmail.com>
2022-03-16 17:26:02 +0100

    Initial version

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
@@ -2,7 +2,6 @@ package com.kodilla.collections.adv.exercises.homework;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,23 +12,14 @@ public class FlightFinderTestSuite {
 
         List<Flight> resultDeparture = FlightFinder.findFlightsFrom("Warsaw");
 
-        List<Flight> expectedDeparture = new ArrayList<>();
-        expectedDeparture.add(new Flight("Warsaw", "Gdansk"));
-        expectedDeparture.add(new Flight("Warsaw", "Berlin"));
-        expectedDeparture.add(new Flight("Warsaw", "Frankfurt"));
         assertEquals(3, resultDeparture.size());
 
         List<Flight> resultDeparture1 = FlightFinder.findFlightsFrom("Gdansk");
 
-        List<Flight> expectedDeparture1 = new ArrayList<>();
-        expectedDeparture1.add(new Flight("Gdansk", "Warsaw"));
         assertEquals(1,resultDeparture1.size());
 
         List<Flight> resultDeparture2 = FlightFinder.findFlightsFrom("Berlin");
 
-        List<Flight> expectedDeparture2 = new ArrayList<>();
-        expectedDeparture2.add(new Flight("Berlin", "Gdansk"));
-        expectedDeparture2.add(new Flight("Berlin", "Warsaw"));
         assertEquals(2,resultDeparture2.size());
 
     }
@@ -38,28 +28,18 @@ public class FlightFinderTestSuite {
     public void testFindFlightsTo () {
         List<Flight> resultArrival = FlightFinder.findFlightsTo("Warsaw");
 
-        List<Flight> expectedArrival = new ArrayList<>();
-        expectedArrival.add(new Flight("Gdansk", "Warsaw"));
-        expectedArrival.add(new Flight("Berlin", "Warsaw"));
         assertEquals(2,resultArrival.size());
 
         List<Flight> resultArrival1 = FlightFinder.findFlightsTo("Gdansk");
 
-        List<Flight> expectedArrival1 = new ArrayList<>();
-        expectedArrival1.add(new Flight("Warsaw", "Gdansk"));
-        expectedArrival1.add(new Flight("Berlin", "Gdansk"));
         assertEquals(2,resultArrival1.size());
 
         List<Flight> resultDeparture2 = FlightFinder.findFlightsTo("Berlin");
 
-        List<Flight> expectedArrival2 = new ArrayList<>();
-        expectedArrival2.add(new Flight("Warsaw", "Berlin"));
         assertEquals(1,resultDeparture2.size());
 
         List<Flight> resultDeparture3 = FlightFinder.findFlightsTo("Frankfurt");
 
-        List<Flight> expectedArrival3 = new ArrayList<>();
-        expectedArrival3.add(new Flight("Warsaw", "Frankfurt"));
         assertEquals(1,resultDeparture3.size());
     }
 }
