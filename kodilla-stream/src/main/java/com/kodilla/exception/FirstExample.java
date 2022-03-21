7ceda65f0f07eb62a8a1cd13c849fb5e5eef1cdf
Pawel Kazubek <p.kazubek@gmail.com>
2022-03-21 22:15:34 +0100

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/exception/FirstExample.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/FirstExample.java
@@ -2,7 +2,7 @@ package com.kodilla.exception;
 
 public class FirstExample {
     public static void main(String[] args) {
-        String username = "Willy Wonka";
+        String username = "John";
         String[] nameParts = username.split(" ");
         System.out.println("Firstname: " + nameParts[0]);
         System.out.println("Lastname: " + nameParts[1]);
---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.homework;
+
+public class WarehouseApp {
+    public static void main(String[] args) throws OrderDoesntExistException {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("Order nr 1"));
+        warehouse.addOrder(new Order("Order nr 2"));
+        warehouse.addOrder(new Order("Order nr 3"));
+        warehouse.addOrder(new Order("Order nr 4"));
+        warehouse.addOrder(new Order("Order nr 5"));
+
+        try {
+            warehouse.isOrderInUse("Order nr 6");
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Such order doesn't exist. Please try another number.");
+        } finally {
+            System.out.println("You have got your order. Please check another one.");
+        }
+    }
+}
