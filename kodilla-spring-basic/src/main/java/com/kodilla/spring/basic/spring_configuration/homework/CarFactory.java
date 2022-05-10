93c4c12468a30c50751521ed04edcb85d5d1f774
Pawel Kazubek <p.kazubek@gmail.com>
2022-05-10 16:11:56 +0200

    Initial version

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -9,29 +9,29 @@ import java.time.LocalTime;
 @Configuration
 public class CarFactory {
 
-    LocalDate month = LocalDate.now();
-    LocalTime hour = LocalTime.now();
+  //  LocalDate month = LocalDate.now();
 
-    public CarFactory(LocalDate month, LocalTime hour) {
-        this.month = month;
-        this.hour = hour;
-    }
+   // public CarFactory(LocalDate month) {
+     //   this.month = month;
+   // }
 
     @Bean
     public SUV createSUV() {
         return new SUV();
     }
 
-    @Bean
-    public Car randomCar() {
-        Car car;
-        if (month.getMonth() == 12 & (month.getMonth() == 1) && (month.getMonth() == 2)) && (hour.getHour() == (20 - 6)) {
-            car = new SUV();
-        } else if (LocalDate.now().getMonth(2, 3, 4, 8, 9, 10)) && (LocalTime.now().getHour(20.00 - 6.00))) {
-            car = new Kombi();
-        } else (LocalDate.now().getMonth(5, 6, 7)) && (LocalTime.now().getHour(20.00 - 6.00))) {
-            car = new Cabrio();
-        }
-        return car;
-    }
+   // @Bean
+    //public Car randomCar() {
+        //Car car;
+        //if ((month.getMonthValue() == 12) && (month.getMonthValue() == 1) && (month.getMonthValue() == 2))
+            // {
+          //  car = new SUV();
+        //} else if ((month.getMonthValue() == 3) && (month.getMonthValue() == 4) && (month.getMonthValue() == 8) &&
+               // (month.getMonthValue() == 9) && (month.getMonthValue() == 10) && (month.getMonthValue() == 11))  {
+           // car = new Kombi();
+       // } else ((month.getMonthValue() == 5) && (month.getMonthValue() == 6) && (month.getMonthValue() == 7)) {
+           // car = new Cabrio();
+       // }
+       // return car;
+   // }
 }
